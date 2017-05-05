f29ed2db382c23eeb991427d78e5a916630f6ebd
Rose Howell <howell.rose7@gmail.com>
2017-05-04 22:23:19 -0400

    Finished the basic goroutine structure and added CLI worker flag

--- smtpConnection.go
+++ smtpConnection.go
@@ -16,10 +16,11 @@ import (
     "os"
     // "reflect"
     "sync"
+    "flag"
 )
 
 type ICMPPacket struct {
-    valid       bool
+    Valid       bool
     // TargetIPv4  net.IP
     // ReachedIPv4 net.Addr
     TargetIPv4  string
@@ -29,7 +30,9 @@ type ICMPPacket struct {
 
 func main() {
     //---------------------------- Set up ------------------------------//
-    workers := 10 // number of threads, should be a flag // ./smtp --workers 10
+    // workers := 1 // number of threads, should be a flag // ./smtp --workers 10
+    var workers int
+    flag.IntVar(&workers, "workers", 1, "number of worker routines")
     minTTL := 10
     maxTTL := 26
     w := new(sync.WaitGroup)
@@ -117,8 +120,8 @@ func main() {
                 fmt.Println("REACHED: peer: ", peer)
                 fmt.Print("DATA: ")
 
-                // icmp_parsed := ICMPPacket{TargetIPv4: header.Dst.To4(), ReachedIPv4: peer.(*net.TCPAddr).IP.To4(), valid: true}
-                icmp_parsed := ICMPPacket{TargetIPv4: header.Dst.String(), ReachedIPv4: peer.(*net.TCPAddr).IP.String(), valid: true}
+                // icmp_parsed := ICMPPacket{TargetIPv4: header.Dst.To4(), ReachedIPv4: peer.(*net.TCPAddr).IP.To4(), Valid: true}
+                icmp_parsed := ICMPPacket{TargetIPv4: header.Dst.String(), ReachedIPv4: peer.(*net.TCPAddr).IP.String(), Valid: true}
 
 
                 icmp_extensions := body.Extensions
@@ -139,7 +142,7 @@ func main() {
             default:
                 // log.Printf("unknown ICMP message: %+v\n", rm)
                 fmt.Println("ICMP response unknown/default/other")
-                default_icmp_packet := ICMPPacket{valid: false}
+                default_icmp_packet := ICMPPacket{Valid: false}
                 icmp_chan <- default_icmp_packet
             } //  switch
         } // for
@@ -157,7 +160,7 @@ func main() {
             m.Lock() // lock accesses to in_process map
             defer m.Unlock()
             target := packet.TargetIPv4 // grab the target IP from the parsed ICMP 
-            if c, ok := in_process[target]; ok { // if we have a worker for that target
+            if _, ok := in_process[target]; ok { // if we have a worker for that target
                 in_process[target] <- packet // tell the worker we got that packet
             }
         }
@@ -168,7 +171,7 @@ func main() {
 
 
     // -------TODO: get the next IP address from a global queue ---------------------//
-    target := "mail.rchowell.net:25"
+    // target := "mail.rchowell.net:25"
     // target := "128.32.80.167:25"
     // target := "173.194.69.26:25"
     // target := "128.32.78.35:25"
@@ -180,141 +183,207 @@ func main() {
 
 
 
-    
 
-
-    //------------------------- Open the connection --------------------------//
-    fmt.Println("Attempting connection to: ", target)
-    timeOut := time.Duration(5) * time.Second
-    // Dial: returns a new Client connected to a server at addr
-    // conn, err := net.Dial("tcp", target)
-    conn, err := net.DialTimeout("tcp", target, timeOut)
-    if err != nil {
-        fmt.Println("dial error:", err)
-        return
-    }
-    defer fmt.Println("Closing Connection, test")
-    defer conn.Close()
-    
-    // Wait for 220 banner
-    banner, err := bufio.NewReader(conn).ReadString('\n')
-    fmt.Println(banner)
-
-    // Are we being greylisted/blacklisted?
-    bannerGood, err := regexp.MatchString("220 ", string(banner))
-
-    if !bannerGood {
-        fmt.Println("This server did not give us a good banner: ", target)
-        return
+    //------------------------------ Worker Goroutine ------------------------//
+    worker_channels := make([]chan ICMPPacket, workers)
+    for idx := range worker_channels {
+        worker_channels[idx] = make(chan ICMPPacket, 1)
     }
 
-    //---------------------- Send EHLO, receive Extensions -------------------//
-    // time.Sleep(100 * time.Millisecond)
-    conn.Write([]byte("EHLO ME\r\n"))
-    // while "250-*", reading lines. if "250 *", break
-    bufReader := bufio.NewReader(conn)
-    for {
-        // Read tokens delimited by newline
-        extensions, err := bufReader.ReadBytes('\n')
-        if err != nil { // if there's an error
-            fmt.Println()
-            break
-        }
+    for worker := 0; worker < workers; worker++ {
+        w.Add(1)
+        c := worker_channels[worker]
+        go func(c chan ICMPPacket) {
+            defer w.Done()
+            for ip := range input_chan {
+                //------------- Get an IP Address from input_chan ------------//
+                m.Lock()
+                in_process[ip] = c
+                m.Unlock()
+                defer func() {
+                    m.Lock()
+                    delete(in_process, ip)
+                    m.Unlock()
+                }()
 
-        fmt.Printf("%s", extensions)
+                target := ip+":25"
+                // fmt.Println("target: ")
 
-        matched, err := regexp.MatchString("250 ", string(extensions))
-        if matched {
-            // fmt.Println("Found the last extension\n")
-            fmt.Println()
-            break
-        }
+                //------------------------ Open the connection -----------------------//
+                fmt.Println("Attempting connection to: ", target)
+                timeOut := time.Duration(5) * time.Second
+                // Dial: returns a new Client connected to a server at addr
+                // conn, err := net.Dial("tcp", target)
+                conn, err := net.DialTimeout("tcp", target, timeOut)
+                if err != nil {
+                    fmt.Println("dial error:", err)
+                    return
+                }
+                defer fmt.Println("Closing Connection, test")
+                defer conn.Close()
+                
+                // Wait for 220 banner
+                banner, err := bufio.NewReader(conn).ReadString('\n')
+                fmt.Println(banner)
 
-        // 500 5.5.1 Command unrecognized: "XXXX ME"
-        ehloError, err := regexp.MatchString("500 ", string(extensions))
-        if ehloError {
-            fmt.Println("got a 500 error: ", string(extensions))
-            fmt.Println("Sending 'HELO' instead")
-            conn.Write([]byte("HELO ME\r\n"))
-        }
-    } // for
+                // Are we being greylisted/blacklisted?
+                bannerGood, err := regexp.MatchString("220 ", string(banner))
+
+                if !bannerGood {
+                    fmt.Println("This server did not give us a good banner: ", target)
+                    return
+                }
+
+                //---------------------- Send EHLO, receive Extensions -------------------//
+                // time.Sleep(100 * time.Millisecond)
+                conn.Write([]byte("EHLO ME\r\n"))
+                // while "250-*", reading lines. if "250 *", break
+                bufReader := bufio.NewReader(conn)
+                for {
+                    // Read tokens delimited by newline
+                    extensions, err := bufReader.ReadBytes('\n')
+                    if err != nil { // if there's an error
+                        fmt.Println()
+                        break
+                    }
+
+                    fmt.Printf("%s", extensions)
+
+                    matched, err := regexp.MatchString("250 ", string(extensions))
+                    if matched {
+                        // fmt.Println("Found the last extension\n")
+                        fmt.Println()
+                        break
+                    }
+
+                    // 500 5.5.1 Command unrecognized: "XXXX ME"
+                    ehloError, err := regexp.MatchString("500 ", string(extensions))
+                    if ehloError {
+                        fmt.Println("got a 500 error: ", string(extensions))
+                        fmt.Println("Sending 'HELO' instead")
+                        conn.Write([]byte("HELO ME\r\n"))
+                    }
+                } // for
 
 
-    // //----------------- Test Tegular STARTTLS request ---------------------//
-    // fmt.Println("Sending STARTTLS")
-    // conn.Write([]byte("STARTTLS\r\n"))
-    // tlsResponse, err := bufio.NewReader(conn).ReadBytes('\n')
-    // fmt.Println(string(tlsResponse))
-    // // fmt.Println(err)
-
-    // starttlsGood, err := regexp.MatchString("220 ", string(tlsResponse))
-    // starttlsBad, err := regexp.MatchString("500 ", string(tlsResponse))
-
-    // if starttlsGood {
-    //     fmt.Println("This server is good to start TLS: ", target)
-    // } else if starttlsBad {
-    //     fmt.Println("This server is unable to start TLS: ", target)
-    // } else {
-    //     fmt.Println("Response did not contain 220 or 500, trying again")
-    //     conn.Write([]byte("STARTTLS\n"))
-    //     tlsResponse, err = bufio.NewReader(conn).ReadBytes('\n')
-    //     fmt.Println(string(tlsResponse))
-    // }
-
-
-    
-    //--------------------- Send magic StartTLS packets ----------------------//
-    // Make a new ipv4 connection from the original one
-    startTlsConn := ipv4.NewConn(conn)
-    if err != nil {
-        fmt.Println("Forking conn error:", err)
-        return
-    }
-    defer startTlsConn.Close()
-
-    // Send the fudged StartTLS packets
-    ttl := minTTL
-    for ; ttl < maxTTL; ttl++ {
-        startTlsConn.SetTTL(ttl)
-        // fmt.Fprintf(startTlsConn, "STARTTLS\r\n") // send packet
-        startTlsConn.Write([]byte("STARTTLS\r\n"))
-        fmt.Println("\nSent STARTTLS packet with TTL: ", ttl)
-
-        // TODO: listen for reply, wait for signal from ICMP listener
-        // time.Sleep(100 * time.Millisecond)
-        fmt.Println("224: waiting for icmp_chan")
-        starttlsResponse := <-icmp_chan
-        fmt.Println("226: got icmp_chan")
-        if starttlsResponse == "ttl_expired" {
-            fmt.Println("TTL expired, loop again")
-        } else if starttlsResponse == "unknown" {
-            fmt.Println("unknown response, loop again")
-        } else if starttlsResponse == "echo" {
-            fmt.Println("echo response, loop again")
-        }else {
-            fmt.Println("listen for the normal STARTTLS response now")
-            tlsResponse, err := bufio.NewReader(conn).ReadBytes('\n')
-            if err != nil {
-                fmt.Println("Error listening for StartTLS response: ", err)
-            }
-            fmt.Println(string(tlsResponse))
-
-            starttlsGood, err := regexp.MatchString("220 ", string(tlsResponse))
-            starttlsBad, err := regexp.MatchString("500 ", string(tlsResponse))
-
-            if starttlsGood {
-                fmt.Println("This server is good to start TLS: ", target)
-            } else if starttlsBad {
-                fmt.Println("This server is unable to start TLS: ", target)
-            } else {
-                fmt.Println("Response did not contain 220 or 500, trying again")
-                // conn.Write([]byte("STARTTLS\n"))
-                // tlsResponse, err = bufio.NewReader(conn).ReadBytes('\n')
+                // //----------------- Test Tegular STARTTLS request ---------------------//
+                // fmt.Println("Sending STARTTLS")
+                // conn.Write([]byte("STARTTLS\r\n"))
+                // tlsResponse, err := bufio.NewReader(conn).ReadBytes('\n')
                 // fmt.Println(string(tlsResponse))
-            }
-        }
+                // // fmt.Println(err)
 
-    }
+                // starttlsGood, err := regexp.MatchString("220 ", string(tlsResponse))
+                // starttlsBad, err := regexp.MatchString("500 ", string(tlsResponse))
+
+                // if starttlsGood {
+                //     fmt.Println("This server is good to start TLS: ", target)
+                // } else if starttlsBad {
+                //     fmt.Println("This server is unable to start TLS: ", target)
+                // } else {
+                //     fmt.Println("Response did not contain 220 or 500, trying again")
+                //     conn.Write([]byte("STARTTLS\n"))
+                //     tlsResponse, err = bufio.NewReader(conn).ReadBytes('\n')
+                //     fmt.Println(string(tlsResponse))
+                // }
+
+
+                
+                //--------------------- Send magic StartTLS packets ----------------------//
+                tcpResponseChan := make(chan []byte)
+                hostTimeoutChan := make(chan bool)
+
+                go func() { // TCP Listener
+                    timeoutDuration := 5 * time.Second
+                    bufReader := bufio.NewReader(conn)
+                    conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+                    res, err := bufReader.ReadBytes('\n')
+                    // res, err := ReadNormalStartTLSAnswerWithMaxTimeout(conn)
+                    if err != nil {
+                        fmt.Println(err)
+                        hostTimeoutChan <- true
+                    }
+                    tcpResponseChan <- res
+                }() // TCP Listener
+
+                hostDone := false
+                // ttlCount := 0
+
+
+
+                // Make a new ipv4 connection from the original one
+                startTlsConn := ipv4.NewConn(conn)
+                if err != nil {
+                    fmt.Println("Forking conn error:", err)
+                    return
+                }
+                defer startTlsConn.Close()
+
+                // Send the fudged StartTLS packets
+                ttl := minTTL
+                for ; ttl < maxTTL && !hostDone; ttl++ {
+                    startTlsConn.SetTTL(ttl)
+                    // fmt.Fprintf(startTlsConn, "STARTTLS\r\n") // send packet
+                    startTlsConn.Write([]byte("STARTTLS\r\n"))
+                    fmt.Println("\nSent STARTTLS packet with TTL: ", ttl)
+                    hopDone := false
+
+
+                    for !hopDone && !hostDone {
+                        select {
+                            case <- c:
+                                // got an icmp packet
+                                hopDone = true
+                                // check for censorship in echo'd response
+                            case <- tcpResponseChan:
+                                hostDone = true
+                                // ttlCount = ttl
+                            case <-time.After(3 * time.Second):
+                                hopDone = true
+                                continue
+                            case <- hostTimeoutChan:
+                                hostDone = true
+
+                        } // select
+                    }
+
+
+                    // TODO: listen for reply, wait for signal from ICMP listener
+                    // time.Sleep(100 * time.Millisecond)
+                    // fmt.Println("224: waiting for icmp_chan")
+                    // starttlsResponse := <-icmp_chan
+                    // fmt.Println("226: got icmp_chan")
+                    // if starttlsResponse == "ttl_expired" {
+                    // if starttlsResponse.Valid {
+                    //     fmt.Println("TTL expired, loop again")
+                    // } else {
+                    //     fmt.Println("listen for the normal STARTTLS response now")
+                    //     tlsResponse, err := bufio.NewReader(conn).ReadBytes('\n')
+                    //     if err != nil {
+                    //         fmt.Println("Error listening for StartTLS response: ", err)
+                    //     }
+                    //     fmt.Println(string(tlsResponse))
+
+                    //     starttlsGood, err := regexp.MatchString("220 ", string(tlsResponse))
+                    //     starttlsBad, err := regexp.MatchString("500 ", string(tlsResponse))
+
+                    //     if starttlsGood {
+                    //         fmt.Println("This server is good to start TLS: ", target)
+                    //     } else if starttlsBad {
+                    //         fmt.Println("This server is unable to start TLS: ", target)
+                    //     } else {
+                    //         fmt.Println("Response did not contain 220 or 500, trying again")
+                    //         // conn.Write([]byte("STARTTLS\n"))
+                    //         // tlsResponse, err = bufio.NewReader(conn).ReadBytes('\n')
+                    //         // fmt.Println(string(tlsResponse))
+                    //     }
+                    // }
+                }
+            } // for ip := range input_chan
+            close(c)
+        }(c) // Worker goroutine
+    } // for worker < workers
+    w.Wait()
 
     //------------------------ Close the connection --------------------------//
     // time.Sleep(1 * time.Second)
